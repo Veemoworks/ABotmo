8a6b494607eb994de8bb9b17baebd444912ba1a9
AVeemo <yousuf4210@gmail.com>
2025-08-10 09:43:01 +0100

    fix dict, bug fixes, and structure

--- Cogs/Classes/DiscordViews.py
+++ Cogs/Classes/DiscordViews.py
@@ -1,7 +1,6 @@
 import discord
 from Cogs.Classes.DiscordButtons import PrefixChange
-from Cogs.Classes.DiscordSelects import Logs
-from DataBases.database import server_roles
+from DataBases.database import server_roles, modlogchannel
 from discord.ui import View, Select
 
 # All of discord.ui.view here
@@ -16,7 +15,7 @@ class Config(View):
         self.configured_roles = configured_roles
         guild_roles = interaction.guild.roles
         guild_roles = [r for r in guild_roles if r.name != "@everyone"]
-        guild_channels = interaction.guild.channels
+        guild_channels = interaction.guild.text_channels
         guild_channels = [r for r in guild_channels]
 
         for chunk in chunk_list(guild_roles, 25):
@@ -31,6 +30,46 @@ class Config(View):
         await self.interaction.edit_original_response(view=self)
 
 # ui.Select here due to a circular import in Cogs.Classes.DiscordSelects.py
+class Logs(Select):
+    def __init__(self, all_channels, configured_channel):
+        options = []
+        for channel in all_channels:
+            if not channel.is_news() and not channel.is_nsfw():
+                options.append(
+                    discord.SelectOption(
+                        label=channel.name,
+                        value=str(channel.id),
+                        default=str(channel.id) in configured_channel
+                    )
+                )
+        super().__init__(
+            placeholder="Channel",
+            min_values=0,
+            max_values=1,
+            options=options
+        )
+
+    async def callback(self, interaction: discord.Interaction):
+        current_roles = set(modlogchannel(False, interaction))
+        selected_roles = set(self.value)
+
+        added_roles = selected_roles - current_roles
+        removed_roles = (set(opt.value for opt in self.options) & current_roles) - selected_roles
+
+        changes = []
+        for role_id in added_roles.union(removed_roles):
+            msg = modlogchannel(True, interaction, role_id)
+            changes.append(msg)
+
+        new_config = modlogchannel(False, interaction)
+        view = Config(interaction, new_config)
+        change_summary = "\n".join(changes) if changes else "No changes made."
+
+        await interaction.response.edit_message(
+            content=change_summary,
+            view=view
+        )
+
 class Role(Select):
     def __init__(self, all_roles, configured_roles):
         options = []
