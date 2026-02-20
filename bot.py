96f23a9f8b9c1b6790f26710b337e27695839c14
AVeemo <yousuf4210@gmail.com>
2026-02-21 01:31:04 +0400

    changed v num to 0.7.3 to match patch bump

--- bot.py
+++ bot.py
@@ -10,7 +10,7 @@ from Cogs.Methods.methods import handle_exception, log, close_bot
 from resources.dictionaries import custom_urls
 
 # Variables
-version = "0.7.2"
+version = "0.7.3"
 pid = os.getpid()
 debugging = False
 done = False
