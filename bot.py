88dc6808f37b4695e555aa93756ac0971dbef5ba
AVeemo <yousuf4210@gmail.com>
2026-03-16 06:09:06 +0400

    made init file way smaller cuz why not

--- bot.py
+++ bot.py
@@ -1,25 +1,15 @@
-import discord, os, sys, logging, asyncio
-from discord.ext import commands
+import os, asyncio, datetime, discord.ext, dotenv
 from Cogs.BotEvents import load
-from datetime import datetime
-from dotenv import load_dotenv
 from Cogs.Methods.asynchronous.methods import get_prefix
-from Cogs.Methods.methods import handle_exception, log
 
-debugging = False
-startup = datetime.now()
-load_dotenv()
-sys.excepthook = handle_exception
+startup = datetime.datetime.now()
 
 if __name__ == "__main__":
+    dotenv.load_dotenv()
     with open("output.txt", "w") as f:
-        f.write("")
-        print(log(False, "Initilizaing..."))
-    if debugging:
-        with open("debug.txt", "w") as f:
-            f.write("")
-    # for sharded client (1000+ servers)
-    # bot = commands.AutoShardedBot
-    bot = commands.Bot(command_prefix=get_prefix, intents=discord.Intents.all(), max_messages=500)
+        msg = f"[{startup.strftime("%d-%m-%Y %H:%M:%S")}] [INFO    ] Initilizaing..."
+        f.write(msg + "\n")
+        print(f"\033[92m{msg}")
+    bot = discord.ext.commands.Bot(get_prefix, intents=discord.Intents.all(), max_messages=500)
     asyncio.run(load(bot))
-    bot.run(os.getenv("TOKEN"), log_handler=logging.FileHandler(filename='debug.txt', encoding='utf-8', mode='a') if debugging else None, log_level=logging.DEBUG)
+    bot.run(os.getenv("TOKEN"), log_handler=None)
