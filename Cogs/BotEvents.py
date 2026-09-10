33a46adf2a0aae2ed36a94b3365e6e5d9e138da7
veraveemo <yousuf4210@gmail.com>
2026-09-10 10:29:21 +0100

    woops forgot to put right variable

--- Cogs/BotEvents.py
+++ Cogs/BotEvents.py
@@ -51,7 +51,7 @@ class Events(commands.Cog):
                     ramthing.start()
                     status.start(self.bot)
                     # kuma.start(self.bot)
-                    setupBotEmbeds(client)
+                    setupBotEmbeds(self.bot)
                     t = self.bot.get_channel(1403041372751265912)
                     await t.edit(name="[ 🟢 ] | Bot Status: Online")
                     print(log(False, f"READY TO KILL ALL FELLAS..."))
